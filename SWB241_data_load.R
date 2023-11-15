41598410d0340232a21c32b484f2b0c33fa1359a
Olivia <oliviamccargar@gmail.com>
2023-11-15 10:50:54 -0500

    Added clean column names .csvs, rewrite procedure for column names

--- SWB241_data_load.R
+++ SWB241_data_load.R
@@ -10,5 +10,16 @@ results_2019 <- read_excel(pth)
 pth <- file.path(getwd(), "data", "2023 Members survey full download.csv")
 results_2023 <- read.csv(pth)
 
-results_2023 %>% colnames()
-results_2019 %>% colnames()
+pth <- file.path(getwd(), "data", "2019 Column Rename.csv")
+columns_2019 <- read.csv(pth)
+
+pth <- file.path(getwd(), "data", "2023 Column Rename.csv")
+columns_2023 <- read.csv(pth)
+
+results_2019 <- results_2019 %>%
+  setNames(columns_2019[["clean_column"]])
+
+results_2023 <- results_2023 %>%
+  setNames(columns_2023[["clean_column"]])
+
+
