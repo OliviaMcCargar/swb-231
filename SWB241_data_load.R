e3ccb6422e72dddd1b285e5a4844b7f733013791
Olivia <oliviamccargar@gmail.com>
2023-11-15 17:10:16 -0500

    Added some comments to clarify what exactly is being done in which section, eliminated false header in 2019 data set

--- SWB241_data_load.R
+++ SWB241_data_load.R
@@ -4,6 +4,7 @@
 library(readxl)
 library(tidyverse)
 
+# Load the raw data and updated column names
 pth <- file.path(getwd(), "data", "2019 Members survey full data.xlsx")
 results_2019 <- read_excel(pth)
 
@@ -16,6 +17,8 @@ columns_2019 <- read.csv(pth)
 pth <- file.path(getwd(), "data", "2023 Column Rename.csv")
 columns_2023 <- read.csv(pth)
 
+
+# Overwrite the raw column names with more descriptive alternatives
 results_2019 <- results_2019 %>%
   setNames(columns_2019[["clean_column"]])
 
@@ -23,3 +26,5 @@ results_2023 <- results_2023 %>%
   setNames(columns_2023[["clean_column"]])
 
 
+# Remove false header row generated from output
+results_2019 <- results_2019[-1,]
